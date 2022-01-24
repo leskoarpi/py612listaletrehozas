0b17203210cd27e3b6191dca0db69388c518b9bc
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-24 10:49:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+2.1 Feladat
+Készíts egy programot, amely a felhasználótól kis "a" betűvel kezdődő szavakat kér be és ezeket tárolja. Ha a felhasználó nem "a" betűvel kezdődő szót ad meg, akkor azt hagyja figyelmen kívül és ne tárolja. A bekérés egészen addig folytatódjon, amíg a felhasználó ENTER-t nem üt (nem ad meg újabb nevet a bekérésnél)! A program a bekért neveket írja ki a képernyőre!
+
+A program tájékoztatássa a felhasználót a működéséről, és az elvárt adatokról!
+
+2.2 Feladat
+Alakítsd át úgy az előző porgramot, hogy az ne csak kis, hanem a nagy "A" betűvel kezdődő szavakat is elfogadja.
+'''
+lista=[]
+'''
+while True:
+    beker = input('irj be egy "a"-val kezdodo szót: ')
+    if beker == '':
+        break
+    elif beker[0] == 'a':
+        lista.append(beker)
+   
+print(lista)'''
+
+while True:
+    beker = input('irj be egy "a"-val kezdodo szót: ')
+    if beker == '':
+        break
+    elif beker[0] == 'a' or beker[0] == 'A':
+        lista.append(beker)
+   
+print(lista)
